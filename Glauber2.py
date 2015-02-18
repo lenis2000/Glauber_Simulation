60030f9a7d0c9ccc0d1334ba70330dfb3a2c6176
Leonid Petrov <lenia.petrov@gmail.com>
2015-02-18 10:21:54 -0500

    fixed initial data

--- Glauber2.py
+++ Glauber2.py
@@ -1,9 +1,9 @@
 import numpy
 numpy.set_printoptions(threshold=numpy.nan)
 
-###DEPTH OF THE INTERLACING ARRAY
 
-n = 6
+n = 16					###DEPTH OF THE INTERLACING ARRAY
+sim_steps = 10000 		###Number of Glauber steps per one simulation
 
 la = numpy.zeros((n,n))
 
@@ -11,12 +11,12 @@ la = numpy.zeros((n,n))
 
 ###HEXAGON
 
-for m in xrange(0,n):
-	for j in xrange(0,m+1):
-		if n/2 <= m <= n and 0 <= j <= m - n/2:
-			la[m][j] = n/2 + 1
-		else:
-			la[m][j] = 1
+# for m in xrange(0,n):
+# 	for j in xrange(0,m+1):
+# 		if n/2 <= m <= n and 0 <= j <= m - n/2:
+# 			la[m][j] = n/2 + 1
+# 		else:
+# 			la[m][j] = 1
 
 ###CARDIOID
 
@@ -25,24 +25,25 @@ for m in xrange(0,n):
 # 		if 2*n/3 <= m <= n and 0 <= j <= m - 2*n/3:
 # 			la[m][j] = 2*n + 1
 # 		else:
-# 			if 2*n/3 <= m <= n and m - 2*n/3 < j <= m - n/3:
+# 			if n/3 <= m <= n and m - 2*n/3 < j <= m - n/3:
 # 				la[m][j] = 3*n/2 + 1
 # 			else:
+# 				la[m][j] = 1
 
 ###12-GON
 
-# for m in xrange(0,n):
-# 	for j in xrange(0,m+1):
-# 		if 3*n/4 <= m <= n and 0 <= j <= m - 3*n/4:
-# 			la[m][j] = 3*n/2 + 1
-# 		else:
-# 			if 3*n/4 <= m <= n and m - 3*n/4 < j <= m - 2*n/4:
-# 				la[m][j] = n + 1
-# 			else:
-# 				if 3*n/4 <= m <= n and m - 2*n/4 < j <= m - 1*n/4:
-# 					la[m][j] = n/2 + 1
-# 				else:
-# 					la[m][j] = 1
+for m in xrange(0,n):
+	for j in xrange(0,m+1):
+		if 3*n/4 <= m <= n and 0 <= j <= m - 3*n/4:
+			la[m][j] = 3*n/2 + 1
+		else:
+			if 2*n/4 <= m <= n and m - 3*n/4 < j <= m - 2*n/4:
+				la[m][j] = n + 1
+			else:
+				if 1*n/4 <= m <= n and m - 2*n/4 < j <= m - 1*n/4:
+					la[m][j] = n/2 + 1
+				else:
+					la[m][j] = 1
 
 ###INTERACTIVE SIMULATION
 
@@ -73,7 +74,7 @@ while True:
 		f.close()
 
 
-	for x in xrange(1,10000*n):
+	for x in xrange(1,sim_steps*n):
 		
 		m = numpy.random.random_integers(1,n-1)
 		j = numpy.random.random_integers(1,m)
