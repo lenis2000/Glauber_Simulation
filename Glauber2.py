5251afc106a925f6c8a5aa86080ad6b2c9d60a4b
Leonid Petrov <lenia.petrov@gmail.com>
2015-02-19 05:26:46 -0500

    some more work with the sampler

--- Glauber2.py
+++ Glauber2.py
@@ -2,8 +2,8 @@ import numpy
 numpy.set_printoptions(threshold=numpy.nan)
 
 
-n = 40				###DEPTH OF THE INTERLACING ARRAY
-sim_steps = 10000 		###Number of Glauber steps per one simulation
+n = 60				###DEPTH OF THE INTERLACING ARRAY
+sim_steps = 5000		###Number of Glauber steps per one simulation
 
 la = numpy.zeros((n,n))
 
@@ -11,39 +11,53 @@ la = numpy.zeros((n,n))
 
 ###HEXAGON
 
-for m in xrange(0,n):
-	for j in xrange(0,m+1):
-		if n/2 <= m <= n and 0 <= j <= m - n/2:
-			la[m][j] = n/2 + 1
-		else:
-			la[m][j] = 1
+# for m in xrange(0,n):
+# 	for j in xrange(0,m+1):
+# 		if n/2 <= m <= n and 0 <= j <= m - n/2:
+# 			la[m][j] = n/2 + 1
+# 		else:
+# 			la[m][j] = 1
 
-###CARDIOID
+###SYMMETRIC CARDIOID
 
 # for m in xrange(0,n):
 # 	for j in xrange(0,m+1):
 # 		if 2*n/3 <= m <= n and 0 <= j <= m - 2*n/3:
-# 			la[m][j] = n + 1
+# 			la[m][j] = 2*n + 1
 # 		else:
 # 			if n/3 <= m <= n and m - 2*n/3 < j <= m - n/3:
-# 				la[m][j] = n/3 + 1
+# 				la[m][j] = n + 1
+# 			else:
+# 				la[m][j] = 1
+
+###NON-SYMMETRIC CARDIOID
+
+# p=0.4
+
+# for m in xrange(0,n):
+# 	for j in xrange(0,m+1):
+# 		if p*n <= m <= n and 0 <= j <= m - p*n:
+# 			la[m][j] = n + 1
+# 		else:
+# 			if n/3 <= m <= n and m - p*n < j <= m - n/3:
+# 				la[m][j] = 2*n/7 + 1
 # 			else:
 # 				la[m][j] = 1
 
 ###12-GON
 
-# for m in xrange(0,n):
-# 	for j in xrange(0,m+1):
-# 		if 3*n/4 <= m <= n and 0 <= j <= m - 3*n/4:
-# 			la[m][j] = 6*n/2 + 1
-# 		else:
-# 			if 2*n/4 <= m <= n and m - 3*n/4 < j <= m - 2*n/4:
-# 				la[m][j] = 5*n/2 + 1
-# 			else:
-# 				if 1*n/4 <= m <= n and m - 2*n/4 < j <= m - 1*n/4:
-# 					la[m][j] = n/2 + 1
-# 				else:
-# 					la[m][j] = 1
+for m in xrange(0,n):
+	for j in xrange(0,m+1):
+		if 3*n/4 <= m <= n and 0 <= j <= m - 3*n/4:
+			la[m][j] = 5*n/4 + 1
+		else:
+			if 2*n/4 <= m <= n and m - 3*n/4 < j <= m - 2*n/4:
+				la[m][j] = 2*n/2 + 1
+			else:
+				if 1*n/4 <= m <= n and m - 2*n/4 < j <= m - 1*n/4:
+					la[m][j] = n/15 + 1
+				else:
+					la[m][j] = 1
 
 ###INTERACTIVE SIMULATION
 
