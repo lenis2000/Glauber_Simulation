8ab6552dbee06bd223de6ea1b1b2b3a193a8b8e3
Leonid Petrov <lenia.petrov@gmail.com>
2016-07-02 21:41:12 -0400

    doing heart simulation

--- Glauber_heart.py
+++ Glauber_heart.py
@@ -2,9 +2,9 @@ import numpy
 numpy.set_printoptions(threshold=numpy.nan)
 
 
-n = 48			###DEPTH OF THE INTERLACING ARRAY
-sim_steps = 2500	###Number of Glauber steps (times height of the system) per one simulation
-biga = 5
+n = 180			###DEPTH OF THE INTERLACING ARRAY
+sim_steps = 25000	###Number of Glauber steps (times height of the system) per one simulation
+biga = 3300
 
 la = numpy.zeros((n,n))
 laprint = numpy.zeros((n,n))
@@ -15,11 +15,11 @@ laprint = numpy.zeros((n,n))
 
 for m in xrange(0,n):
 	for j in xrange(0,m+1):
-		if 2*n/3 <= m <= n and 0 <= j <= m - 2*n/3:
-			la[m][j] = n + 1
+		if 3*n/4 <= m <= n and 0 <= j <= m - 3*n/4:
+			la[m][j] = 2*n/3 + 1
 		else:
-			if n/3 <= m <= n and m - 2*n/3 < j <= m - n/3:
-				la[m][j] = n/2 + 1
+			if n/4 <= m <= n and m - 3*n/4 < j <= m - n/4:
+				la[m][j] = n/3 + 1
 			else:
 				la[m][j] = 1
 
