5653e00b1b0fe626562b7087a1347fb8fc128e9e
Leonid Petrov <lenia.petrov@gmail.com>
2025-01-27 13:36:37 -0500

    w

--- Glauber2_python3.py
+++ Glauber2_python3.py
@@ -1,88 +1,99 @@
 import numpy as np
 
-# Set numpy print options
-np.set_printoptions(threshold=np.inf)  # np.nan is replaced with np.inf in newer versions
+# In NumPy 1.25+ it’s common to do threshold=None to show the entire array.
+# or np.set_printoptions(threshold=np.inf)
+np.set_printoptions(threshold=None)
 
-# Constants
-n = 30                  # Depth of the interlacing array
-sim_steps = 5000        # Number of Glauber steps (times height of the system) per simulation
+n = 30               # DEPTH OF THE INTERLACING ARRAY
+sim_steps = 5000     # Number of Glauber steps (times height of the system) per one simulation
 biga = 50
 
-# Initialize arrays
 la = np.zeros((n, n))
 laprint = np.zeros((n, n))
 
-# Boundary conditions for hexagon
+# BOUNDARY CONDITIONS
 AAA = n
-addition = -AAA/2-1
+addition = -AAA // 2 - 1
 HOLE_SIZE = 8
 OTHER_ADD = 8
 
-# Make a plain hexagon; initial condition speeds up the convergence
-for m in range(0, n):
-    for j in range(0, m+1):
-        if m >= n/2:
-            if 0 <= j <= m - n/2:
-                la[m][j] = AAA + 1 + addition
-            if j >= n/2:
-                la[m][j] = 1 + addition
+# MAKE A PLAIN HEXAGON ; INITIAL CONDITION SPEEDS UP THE CONVERGENCE
+for m in range(n):
+    for j in range(m + 1):
+        # integer division n//2 used for clarity
+        if m >= n // 2:
+            if 0 <= j <= m - n // 2:
+                la[m, j] = AAA + 1 + addition
+            if j >= n // 2:
+                la[m, j] = 1 + addition
 
-# Main simulation loop
-for a in range(0, biga):
-    # Write current state to file
-    with open(f'outputXX{a}.txt', 'w') as f:
+for a in range(biga):
+    # Write out the current configuration to file
+    with open('outputXX' + str(a) + '.txt', 'w') as f:
         f.write("{")
-        for m in range(0, n):
+        for m in range(n):
             f.write("{")
-            for k in range(0, n):
-                if k == n-1:
-                    f.write(str(la[m][k]))
+            for k in range(n):
+                if k == n - 1:
+                    f.write(str(la[m, k]))
                 else:
-                    f.write(f"{la[m][k]}, ")
-            if m == n-1:
+                    f.write(str(la[m, k]) + ", ")
+            if m == n - 1:
                 f.write("}")
             else:
                 f.write("},")
         f.write("}")
 
-    print(f"{a} ", end="")  # Updated print statement
+    print(a)  # Python 3 style print
 
-    # Simulation steps
-    for x in range(1, sim_steps*n):
-        m = np.random.randint(1, n)  # Updated from random_integers to randint
-        j = np.random.randint(1, m+1)
+    # Perform Glauber updates
+    for x in range(1, sim_steps * n):
+        # random_integers(1, n-1) -> randint(1, n) is the direct replacement:
+        m_rand = np.random.randint(1, n)
+        j_rand = np.random.randint(1, m_rand + 1)  # inclusive of m_rand
 
-        # Apply hole conditions
-        if m == n/2 - 1 and m/2 - HOLE_SIZE <= j <= m/2 + HOLE_SIZE:
-            la[m][j + OTHER_ADD] = OTHER_ADD
-        if m < n/2 - 1 and (n/2 - 1)/2 - HOLE_SIZE <= j <= (n/2 - 1)/2 + HOLE_SIZE - (n/2 - 1 - m):
-            la[m][j + OTHER_ADD] = OTHER_ADD
-        if m > n/2 - 1 and (n/2 - 1)/2 - HOLE_SIZE - (n/2 - 1 - m) <= j <= (n/2 - 1)/2 + HOLE_SIZE:
-            la[m][j + OTHER_ADD] = OTHER_ADD
+        # The "HOLE" conditions:
+        if (m_rand == n // 2 - 1
+            and (m_rand / 2.0 - HOLE_SIZE) <= j_rand <= (m_rand / 2.0 + HOLE_SIZE)):
+            la[m_rand, j_rand + OTHER_ADD] = OTHER_ADD
+        if (m_rand < n // 2 - 1
+            and ((n // 2 - 1) / 2.0 - HOLE_SIZE) <= j_rand
+            and j_rand <= ((n // 2 - 1) / 2.0 + HOLE_SIZE) - (n // 2 - 1 - m_rand)):
+            la[m_rand, j_rand + OTHER_ADD] = OTHER_ADD
+        if (m_rand > n // 2 - 1
+            and ((n // 2 - 1) / 2.0 - HOLE_SIZE - (n // 2 - 1 - m_rand)) <= j_rand
+            and j_rand <= ((n // 2 - 1) / 2.0 + HOLE_SIZE)):
+            la[m_rand, j_rand + OTHER_ADD] = OTHER_ADD
 
-        # Random coin flip and state updates
         coin = np.random.random()
         if coin < 0.5:
-            if la[m-1][j-1] < la[m][j-1]:
-                if m == 1 or j == 1:
-                    la[m-1][j-1] += 1
+            # Attempt "increase" move
+            if la[m_rand - 1, j_rand - 1] < la[m_rand, j_rand - 1]:
+                if m_rand == 1 or j_rand == 1:
+                    la[m_rand - 1, j_rand - 1] += 1
                 else:
-                    if la[m-1][j-1] < la[m-2][j-2]:
-                        la[m-1][j-1] += 1
+                    if la[m_rand - 1, j_rand - 1] < la[m_rand - 2, j_rand - 2]:
+                        la[m_rand - 1, j_rand - 1] += 1
         else:
-            if la[m-1][j-1] > la[m][j]:
-                if m == 1 or j == m:
-                    la[m-1][j-1] -= 1
+            # Attempt "decrease" move
+            if la[m_rand - 1, j_rand - 1] > la[m_rand, j_rand]:
+                if m_rand == 1 or j_rand == m_rand:
+                    la[m_rand - 1, j_rand - 1] -= 1
                 else:
-                    if la[m-1][j-1] > la[m-2][j-1]:
-                        la[m-1][j-1] -= 1
+                    if la[m_rand - 1, j_rand - 1] > la[m_rand - 2, j_rand - 1]:
+                        la[m_rand - 1, j_rand - 1] -= 1
 
-    # Final hole conditions update
-    for m in range(0, n):
-        for j in range(0, n):
-            if m == n/2 - 1 and m/2 - HOLE_SIZE <= j <= m/2 + HOLE_SIZE:
-                la[m][j + OTHER_ADD] = OTHER_ADD
-            if m < n/2 - 1 and (n/2 - 1)/2 - HOLE_SIZE <= j <= (n/2 - 1)/2 + HOLE_SIZE - (n/2 - 1 - m):
-                la[m][j + OTHER_ADD] = OTHER_ADD
-            if m > n/2 - 1 and (n/2 - 1)/2 - HOLE_SIZE - (n/2 - 1 - m) <= j <= (n/2 - 1)/2 + HOLE_SIZE:
-                la[m][j + OTHER_ADD] = OTHER_ADD
+    # Enforce hole boundaries one last time after the simulation steps
+    for m_chk in range(n):
+        for j_chk in range(n):
+            if (m_chk == n // 2 - 1
+                and (m_chk / 2.0 - HOLE_SIZE) <= j_chk <= (m_chk / 2.0 + HOLE_SIZE)):
+                la[m_chk, j_chk + OTHER_ADD] = OTHER_ADD
+            if (m_chk < n // 2 - 1
+                and ((n // 2 - 1) / 2.0 - HOLE_SIZE) <= j_chk
+                and j_chk <= ((n // 2 - 1) / 2.0 + HOLE_SIZE) - (n // 2 - 1 - m_chk)):
+                la[m_chk, j_chk + OTHER_ADD] = OTHER_ADD
+            if (m_chk > n // 2 - 1
+                and ((n // 2 - 1) / 2.0 - HOLE_SIZE - (n // 2 - 1 - m_chk)) <= j_chk
+                and j_chk <= ((n // 2 - 1) / 2.0 + HOLE_SIZE)):
+                la[m_chk, j_chk + OTHER_ADD] = OTHER_ADD
